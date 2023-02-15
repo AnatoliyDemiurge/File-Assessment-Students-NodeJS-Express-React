c1b2d2378cf1e755111ff631df36f208270ca0ff
Anatoly <sharamradov@gmail.com>
2023-02-15 17:58:50 +0300

    added base check similiraty of files, did base creating file

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,7 +1,26 @@
+const fs = require("fs");
 class fileController{
     async createFile(req, res){
-       let name = req.body;
-       return res.json(name);
+        let {name, surname, patronymic, subject, mark} = req.body;
+        let rawFileName = surname + name + patronymic + ".txt";
+        let filesDirestory = '../files/';
+        let fileAddress = filesDirestory + rawFileName;
+        if (fs.existsSync(fileAddress)) {
+            return res.json("Ошибка, нельзя изменить оценку.");
+        }
+        let fileContent = 
+        `Фамилия: ${surname}
+Имя: ${name}
+Отчество: ${patronymic}
+Предмет: ${subject}
+Оценка: ${mark}
+        `
+        fs.writeFile(fileAddress, fileContent, ()=>{
+
+        });
+
+
+        return res.json(fileAddress);
     }
 }
 
